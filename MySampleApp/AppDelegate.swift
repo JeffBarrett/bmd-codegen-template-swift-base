853c497e9d011e985f21285836309fe4b5f4f5d0
Jeff Barrett <barrettj@us.ibm.com>
2016-07-14 11:34:24 -0500

    Move includes tag to the correct place

--- MySampleApp/AppDelegate.swift
+++ MySampleApp/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+// pushIncludes:
+// {{pushIncludes}}
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,8 +18,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-        // pushIncludes:
-        // {{pushIncludes}}
         // pushCoreInit
         // {{pushCoreInit}}
         return true
